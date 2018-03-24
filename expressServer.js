bdadd745f2c0f85ed39705626dce9d47033b3847
ghildreth <gabrielantonhildreth@gmail.com>
2018-03-24 18:17:40 +0000

    works so far, submitting cyprt

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const cookieParser = require('cookie-parser')
 app.use(cookieParser());
 
@@ -10,8 +10,16 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xk': 'http://www.google.com'
+  // b2xVn2: {
+  //   shortURL:'b2xVn2',
+  //   longURL:'http://www.lighthouselabs.ca',
+  //   uid:'bob'
+  // },
+  // '9sm5xk': {
+  //   shortURL: '9sm5xk',
+  //   longURL: 'http://www.google.com',
+  //   uid: 'greg'
+  // }
 };
 
 const users = {
@@ -27,6 +35,17 @@ const users = {
   }
 }
 
+function urlsForEach(user_id) {
+  var newObject = {};
+  for (var key in urlDatabase) {
+    if (user_id === urlDatabase[key].uid) {
+      newObject[key] = urlDatabase[key].longURL;
+    }
+  }
+  return newObject;
+}
+
+
 function generateRandomString() {
   let randoStr = '';
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -34,7 +53,7 @@ function generateRandomString() {
   for (let i = 0; i < 6; i++ ) {
     randoStr += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-      return randoStr;
+  return randoStr;
 }
 
 app.get('/', function (request, response) {
@@ -52,9 +71,10 @@ app.get('/hello', (request, response) => {
 app.get('/urls', (request, response) => {
   let user_id = request.cookies.user_id;
   let user = users[user_id];
+  let newDatabase = urlsForEach(user_id);
 
   const templateVars = {
-    urls: urlDatabase,
+    urls: newDatabase,
     user_id: user_id,
     user: user
 
@@ -62,67 +82,145 @@ app.get('/urls', (request, response) => {
   response.render('urls_index', templateVars);
 });
 
-app.get('/urls/new', (request, response) => {
-   let user = response.cookie('user_id')
-  const templateVars = {longURL: urlDatabase[request.params.id],
-                       user_id: user
-                        };
-  response.render('urls_new', templateVars);
-  console.log(templateVars)
-});
-
-app.get('/urls/:id', (request, response) => {
-  const templateVars = {
-    shortURL: request.params.id,
-    longURL: urlDatabase[request.params.id],
-    user_id: users[currentUser].email
-  };
-  response.render('urls_show', templateVars);
-});
-
-app.get('/u/:shortURL', (request, response) => {
-  const longURL = urlDatabase[request.params.shortURL];
-  response.redirect(longURL);
-});
-
-app.get('/register', (request, response) => {
-  response.render('register');
-});
-
-app.post('/urls', (request, response) => {
-
-
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = request.body.longURL;
-  response.redirect(`/urls/${shortURL}`);
-});
-
-app.post('/urls/:id/update', (request, response) => {
-
- urlDatabase[request.params.id] = request.body.longURL;
- response.redirect(`/urls/${request.params.id}`);
-});
-
 app.get('/login', (request, response) => {
-  console.log("is this working?");
+
   let user_id = request.cookies.user_id;
-  // let user = user_id[user_id];
+  let user = users[user_id];
   let templateVars = {
-      urls: urlDatabase,
-      user: user_id
+      user: user
       };
   response.render('login', templateVars);
 });
 
+app.get('/register', (request, response) => {
+  let user_id = request.cookies.user_id;
+  let user = users[user_id];
+  let templateVars = {
+      user: user
+      };
+  response.render('register', templateVars);
+});
+
+app.get('/urls/new', (request, response) => {
+  let user_id = request.cookies.user_id;
+  let user = users[user_id];
+
+  if (user) {
+    const templateVars = {
+      user: user
+    };
+    response.render('urls_new', templateVars);
+   } else {
+    response.redirect(400, '/login');
+   }
+
+});
+// broke AF
+app.get('/urls/:id', (request, response) => {
+  // added in user_id to access the cookie
+  console.log('does this work');
+  let shortURL = request.params.id;
+  let user_id = request.cookies.user_id;
+  let user = users[user_id];
+  let longURL = urlDatabase[shortURL].longURL;
+  let templateVars = {
+      shortURL: shortURL,
+      longURL: longURL,
+      user: user
+  };
+
+  response.render('urls_show', templateVars);
+});
+
+app.get('/u/:shortURL', (request, response) => {
+  // cannot read property of undefined!
+  let longURL = urlDatabase[request.params.shortURL].longURL;
+  response.redirect(`${longURL}`); // redirects the browser!
+  });
+
+  app.get('/register', (request, response) => {
+  response.render('register');
+});
+
+app.post('/register', (request, response) => {
+  for(let user_id in users) {
+    const user = users[user_id];
+    if (user.email === request.body.email) {
+      return response.redirect(400, '/register');
+    }
+  }
+
+  if(request.body.email && request.body.password) {
+      let currentUserID = generateRandomString();
+
+      users[currentUserID] = {
+        id: currentUserID,
+        email: request.body.email,
+        password: request.body.password
+      };
+      response.cookie('user_id', currentUserID);
+      response.redirect('/urls');
+  } else {
+    response.redirect(400, '/register');
+    response.send("You shall not pass... Please either enter a valid e-mail or password!");
+  }
+
+});
+
+app.post('/urls/:id/delete', (request, response) => {
+  let user_id = request.cookies.user_id;
+  let user = users[user_id];
+  if (user) {
+    let templateVars = {
+      user: user
+    };
+    delete urlDatabase[request.params.id];
+    response.redirect('/urls');
+    return;
+  } else {
+    response.render('urls_new', templateVars);
+  }
+});
+
+app.post('/urls', (request, response) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    shortURL: shortURL, //changed from id:
+    longURL: request.body.longURL, //changed from url
+    uid: request.cookies.user_id //uid:
+  }
+  response.redirect(`/urls/`);
+  console.log(urlDatabase);
+  // don't forget about this thing later maybe ${shortURL}`
+});
+
+app.post('/urls/:id', (request, response) => {
+  // let user = users[user_id]
+  // if (user) {
+  //   let templateVars = {
+  //     user: user
+  //   };
+    urlDatabase[request.params.id] = request.body.longURL;
+    console.log(urlDatabase[request.params.id].longURL);
+    response.redirect('/urls');
+    response.redirect(`/urls/${request.params.id}`);
+  // } else {
+    response.redirect('/login');
+  // }
+
+});
+
+
 app.post('/login', (request, response,) => {
   for (currentUser in users) {
     if ((users[currentUser].email === request.body.email) && (users[currentUser].password === request.body.password)) {
-    response.cookie('user_id', users[currentUser].id);
-    return response.redirect('/urls');
+      response.cookie('user_id', users[currentUser].id);
+      response.redirect('/urls');
+      return;
     }
   }
     response.status(403)
-    return response.redirect('/register');
+    response.redirect('/register');
 
 });
 
@@ -132,39 +230,6 @@ app.post('/logout', (request, response) => {
   response.redirect('/urls');
 
 });
-
-app.post('/register', (request, response) => {
-  let currentUserID = generateRandomString();
-  response.cookie('user_id', currentUserID);
-
-  for(let key in users) {
-    const user = users[key];
-    if (user.email === request.body.email) {
-      return response.sendStatus(400);
-    }
-  }
-
-  if(request.body.email && request.body.password){
-      users[currentUserID] = {
-      id: currentUserID,
-      email: request.body.email,
-      password: request.body.password
-      }
-      response.redirect('/urls');
-
-  } else {
-    response.status(400);
-    response.send("You shall not pass... Please either enter a valid e-mail or password!");
-  }
-
-});
-
-app.post('/urls/:id/delete', (request, response) => {
-  delete urlDatabase[request.params.id];
-  response.redirect('/urls');
-});
-
-
 
 app.listen(PORT, () => {
   console.log(`TURNT MAH HEADPHONES UP TO ${PORT}`);
